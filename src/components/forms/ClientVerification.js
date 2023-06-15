0475ba058cf35859220f02d1b3205cd8a93cdb13
Justus446 <justusoluoch446@gmail.com>
2023-06-15 22:43:46 +0300

    Client verification with client registry

--- src/components/forms/ClientVerification.js
+++ src/components/forms/ClientVerification.js
@@ -1,78 +1,90 @@
-import React from 'react';
 import CountryOptions from '../options/CountryOptions';
+// Client Verification
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import axios from 'axios';
 
 function ClientVerification() {
+  const initialValues = {
+    country: '',
+    IdentifierType: '',
+    identifierNumber: '',
+    selectKenya: false,
+  };
+
+  console.log(localStorage.getItem('token'));
+
+  const searchEndpoint = 'https://afyakenyaapi.health.go.ke/partners/registry/search/KE';
+
+  const handleSubmit = async (values) => {
+    const searchData = {
+      IdentifierType: values.IdentifierType,
+      identifierNumber: values.identifierNumber,
+    };
+    console.log("here 2");
+
+    try {
+      console.log(values);
+      // Make the request to search for the identifierNumber
+
+      const token = localStorage.getItem('token'); // Retrieve the token from local storage
+      console.log(token);
+      // https://afyakenyaapi.health.go.ke/partners/registry/search/KE/birth-certificate/35541176
+      const response = await axios.options(searchEndpoint, searchData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      // Handle the response
+      console.log(response.data);
+    } catch (error) {
+      // Handle the error
+      console.error(error);
+    }
+  };
+
+  const validateForm = (values) => {
+    const errors = {};
+
+    if (!values.IdentifierType) {
+      errors.IdentifierType = 'Identifier Type is required';
+    }
+
+    if (!values.identifierNumber) {
+      errors.identifierNumber = 'Identifier Number is required';
+    }
+
+    return errors;
+  };
+
   return (
     <div>
-      <fieldset id="clientVerificationSection">
+      <fieldset>
         <legend>Client verification with Client Registry</legend>
-        <table>
-          <tbody>
-            <tr>
-              <td className="ke-field-label">Country *</td>
-              <td className="px-2"></td>
-              <td className="px-2"></td>
-              <td className="px-2"></td>
-              <td className="ke-field-label" style={{ whiteSpace: 'nowrap' }}>Identifier Type *</td>
-              <td className="px-2"></td>
-              <td className="px-2"></td>
-            </tr>
-            <tr>
-              <td>
-              <CountryOptions/>
-              </td>
-              <td className="whitespace-nowrap">
-                <input
-                  type="checkbox"
-                  name="select-kenya-option-nupi-verification"
-                  value="Y"
-                  id="select-kenya-option-nupi-verification"
-                />{' '}
-                Select Kenya
-              </td>
-              <td>
-                <div
-                  id="nupi-verification-country-msgBox"
-                  name="nupi-verification-country-msgBox"
-                  className="ke-warning hidden"
-                >
-                  Country is Required
+        <div className="flex items-center justify-center h-screen">
+          <div className="bg-white p-6 rounded shadow-lg">
+            <Formik initialValues={initialValues} validate={validateForm} onSubmit={handleSubmit}>
+              <Form>
+                <div className="mb-4">
+                  <label htmlFor="IdentifierType">Identifier Type:</label>
+                  <Field type="text" id="IdentifierType" name="IdentifierType" />
+                  <ErrorMessage name="IdentifierType" component="div" className="text-red-500" />
+                  <br />
                 </div>
-              </td>
-              <td></td>
-              <td>
-                <select id="idType" name="idtype" className="border rounded p-2">
-                  <option value="">Select a valid identifier type</option>
-                                
-                  <option value="68449e5a-8829-44dd-bfef-c9c8cf2cb9b2">Birth Certificate Number</option>
-                            
-                   <option value="49af6cdc-7968-4abb-bf46-de10d7f4859f">National ID</option>
-                          
-                  <option value="be9beef6-aacc-4e1f-ac4e-5babeaa1e303">Passport Number</option>        
-                      
-                </select>
-              </td>
-              <td>
-                <input type="text" id="idValue" name="idValue" autoComplete="off" className="border rounded p-2" />
-              </td>
-              <td className="ke-field-instructions">
-                <div className="buttons-validate-identifiers">
-                  <button type="button" className="ke-verify-button" id="validate-identifier">
-                    Validate Identifier
-                  </button>
-                  <div className="wait-loading"></div>
-                  <button type="button" className="ke-verify-button hidden" id="show-cr-info-dialog">
-                    View Registry info
-                  </button>
-                  <div className="message-validate-identifiers">
-                    <label id="msgBox"></label>
-                  </div>
+                <p />
+                <div className="mb-4">
+                  <label htmlFor="identifierNumber">Identifier Number:</label>
+                  <Field type="text" id="identifierNumber" name="identifierNumber" />
+                  <ErrorMessage name="identifierNumber" component="div" className="text-red-500" />
+                  <br />
                 </div>
-              </td>
-            </tr>
-            <tr></tr>
-          </tbody>
-        </table>
+
+                <button type="submit">Submit</button>
+                <p />
+                <button type="reset">Reset</button>
+              </Form>
+            </Formik>
+          </div>
+        </div>
       </fieldset>
     </div>
   );
